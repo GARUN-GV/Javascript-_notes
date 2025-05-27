c2ea50a0886cc6a45e94ea353e8bf6085ee2b1e1
Garun Varshney <125990462+GARUN-GV@users.noreply.github.com>
2025-05-27 11:40:25 +0000

    initial commit

---
+++ 01basics/test.js
@@ -0,0 +1,15 @@
+console.log("garun varshney");
+
+
+//js is scripting language which means it is interpreted language rather than a compiled language .
+//  interpreted means the cod eexecuted line by line not exactly once.
+
+
+// js is standalone language which means 
+  // 1. Runs Independently in Browsers
+     // JavaScript can be executed directly by web browsers without the need for any external tools or compilers. For example, you can write and run JavaScript code in the browser’s developer console instantly.
+
+  // 2. Node.js Makes It Work Outside Browsers
+     // With Node.js, JavaScript can run on the server or even build command-line tools, meaning it doesn't rely solely on the browser to function anymore.
+
+  // 3. Self-contained Syntax and Features
