69e8ffe00eaced4c1f4358763ef4f3ed6a405886
Garun Varshney <125990462+GARUN-GV@users.noreply.github.com>
2025-06-29 12:48:13 +0530

    Update 03_conversionOperation.js

--- 01basics/03_conversionOperation.js
+++ 01basics/03_conversionOperation.js
@@ -12,38 +12,39 @@ let valueInNumber = Number(score)
 //console.log(valueInNumber);
 
 
-// "33" => 33
-// "33abc" => NaN
-//  true => 1; false => 0
 
 //if score="22abc"
 //then let valueinnumber=Number(score)
 //console.log(typeof valueinnumber) //number
 //console.log(valueinnumber) //NaN
-//NaN->  
 
-//if score=null
-//then let valueinnumber=Number(score)
+
+/*    NaN->  NaN, which stands for "Not a Number," is a special value in JavaScript that shows up when a mathematical operation can't return a valid number.
+            This can happen if you try something like dividing zero by zero or converting a string that doesn't contain numbers.
+            In JavaScript, NaN stands for "Not-a-Number." 
+            It is a special numeric value that represents an undefined or unrepresentable numerical result, 
+            typically arising from mathematical operations that cannot yield a valid number.
+*/ 
+
+//if score = null
+//then let valueinnumber = Number(score)
 //console.log(typeof valueinnumber) //number
 //console.log(valueinnumber) //0
 
-//if score=undefined
-//then let valueinnumber=Number(score)
+//if score = undefined
+//then let valueinnumber = Number(score)
 //console.log(typeof valueinnumber) //number
 //console.log(valueinnumber) //NaN
 
-//if score=true
-//then let valueinnumber=Number(score)
+//if score  = true
+//then let valueinnumber = Number(score)
 //console.log(typeof valueinnumber) //number
 //console.log(valueinnumber) //1
 
 
-let isLoggedIn = "hitesh"
-
+let isLoggedIn = "Garun"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
-
-
 
 //Conversion of all data type into number and become 
 // "33"=>33
@@ -58,10 +59,12 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "hitesh" => true
 
 let someNumber = 33
-
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// console.log(stringNumber); //33
+// console.log(typeof stringNumber);  //string
+
+
+
 
 // *********************** Operations ***********************
 
@@ -91,11 +94,7 @@ let str3 = str1 + str2
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
-
 let num1, num2, num3
-
 num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
