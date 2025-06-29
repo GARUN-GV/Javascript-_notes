466bf99ef12549a20b52898b0cf2b5d7b4cb7fe0
Garun Varshney <125990462+GARUN-GV@users.noreply.github.com>
2025-06-30 00:10:24 +0530

    Update 06_nums_and_math.js

--- 01basics/06_nums_and_math.js
+++ 01basics/06_nums_and_math.js
@@ -5,11 +5,11 @@ const balance = new Number(100)
 // console.log(balance);
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(1));   //toFixed(1) formats the number with 1 digit after the decimal.
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));    //123.8  //toPrecision(4) formats the number to 4 significant digits, not just digits after the decimal.
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
