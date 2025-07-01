a7822af34026f970d7a423ddb8c64c2b70491d03
Garun Varshney <125990462+GARUN-GV@users.noreply.github.com>
2025-07-01 14:42:13 +0530

    Update strings.js

--- 02_basics/strings.js
+++ 02_basics/strings.js
@@ -7,7 +7,8 @@ const repocount = 50
 
 console.log(name + repocount + " Value");
 
-//concat method-2 (`` string interpolation)
+//concat method-2 (`` string interpolation) =>String interpolation is a method of embedding variables or expressions directly within a string.
+//  It allows you to construct strings dynamically without needing to concatenate them manually.
 console.log(`My name is ${name} and repo count is ${repocount}`)
 
 //new ways to declare the string. in this we use object of js
@@ -22,14 +23,18 @@ const gamename = new String("")
 // const obj={}
 // fetch("url",{obj}).then().catch()
 // new->constructor function
-// new-> it is use to create new context, it create a new copy of object so it doenot override the previous data]
+// new-> it is use to create new context, it create a new copy of object so it doesnot override the previous data]
 
-// step when we use new keyword:
-// 1. first it create a empty new object(instance)
-// 2. second function constructor is called due to new keyword which means argument passinto the main function
-// 3. the argument inject in new keyword
-// 4. deliver into function
+/*      constructor function-> 
+A constructor function is a special type of function in programming used to create and initialize objects.
+It acts as a blueprint or template for creating new instances of a particular type of object, ensuring that each new object starts with a defined initial state
+*/
 
+/*step when we use new keyword:
+1. first it create a empty new object(instance)
+2. second function constructor is called due to new keyword which means argument passinto the main function
+3. the argument inject in new keyword
+4. deliver into function*/
 
-// object.constructor => here object any name of function store variable
+// object1.constructor => here object any name of function store variable
 // instanceof, operator in js
